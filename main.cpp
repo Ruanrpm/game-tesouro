e92dbb194af6133bce62bc0a3792a5c1f50acec1
Ruanrpm <ruan.pablo.martinz@gmail.com>
2024-08-03 16:08:43 -0300

    funções dos eventos terminadas

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 
 // função criar mapa;
-vector<vector<char>> createMapa(int linhas, int colunas) {
+vector<vector<char>> createMapa(const int linhas, const int colunas) {
     
     vector<vector<char>> mapa(linhas, vector<char>(colunas, 79));
-    int aux = 0;
 
     srand(time(0));
 
     // Adiciona mobs e armadilhas
+    int aux = 0;
     while (aux < 6) {
         int I = rand() % linhas;
         int J = rand() % colunas;
@@ -72,6 +72,77 @@ vector<vector<char>> createMapa(int linhas, int colunas) {
     return mapa;
 }
 
+// função caso o jogador encontre uma mumia
+void mumia(int i) {
+    char luta;
+    cout << "Voce encontrou uma mumia quer tentar lutar com ela para roubar a sua bolsa de diamantes?(S)(N): ";
+    cin >> luta;
+    cout << endl;
+    srand(time(0));
+
+    if (luta == 'S') {
+        int lutando = rand() % 10 + 1;
+        if (lutando >= 5) {
+            int tesouro = rand() % 100 +1;
+            if (tesouro == 99) {
+                cout << "Voce derrotou a mumuia e achou um mapa do tesouro na bolsa dela +10000000000 quilates adicionados ao inventario." << endl;
+                quilates[i]+=10000000000;
+            }
+            else {
+                cout << "Voce derrotou a mumia e na bolsa dela tinha 1 diamante de 10 quilates." << endl;
+                    quilates[i]+=10;
+            }
+        }
+        else {
+            if (quilates[i] > 0){
+                quilates[i]=0;
+                cout << "A mumia te derrotou e roubou todos os seus diamantes" << endl;
+            }
+            else{
+                cout << "A mumia te derrotou" << endl;
+            }
+        }
+    }
+    else {
+        cout << "A mumia se afastou" << endl;
+    }
+}
+
+// função caso o jogador caia em uma armadilha de pedra
+void pedra(int i) {
+    if (quilates[i] >= 7) {
+        quilates[i]-=7;
+        cout << "Você caiu em uma armadilha de pedra rolante e ela te esmagou. 7 quilates foram perdidos do seu inventário." << endl;
+    }
+    else {
+        cout << quilates[i] << " Foram perdidos de seu inventário" << endl;
+        quilates[i] = 0;
+    }
+    
+}
+
+// função para armadilha de espinhos;
+
+void espinhos(int i) {
+    if(quilates[i] >= 5) {
+        quilates[i]-=5;
+        cout << "Você caiu em uma armadilha de espinhos e sua bolsa furou. Cinco quilates caíram do seu inventário antes que você percebesse o furo." << endl;
+    }
+    else {
+        quilates[i]=0;
+        cout << quilates[i] << " caíram do seu inventário." <<endl;
+    }
+}
+
+// função para achar diamantes
+
+void diamante (int i) {
+    srand(time(0));
+
+    int diamante = rand() % 10 + 1;
+    cout << "Você achou um diamante de " << diamante << " quilates." << endl;
+    quilates[i] += diamante;
+}
 
 
 
@@ -124,80 +195,77 @@ int main() {
     }
 
     // Criando mapa;
-    int linhas = 6;
-    int colunas = 6;
+    const int linhas = 6;
+    const int colunas = 6;
 
     vector<vector<char>> mapa = createMapa(linhas, colunas);
 
     // corrida;
+    int rodadas = 1;
+    while (rodadas <= ((linhas*colunas)/jogadores)) {
 
-    for (int i = 0; i < linhas;) {
-        for (int j = 0; j < colunas;) {
-
-        //vetor do icone dos jogadores;
-        for (int k = 1; k < jogadores+1;) { 
-            int J = rand() %3 + 1;
-            j+=J;
-            if (j > 6) {
-                i++;
-                j=0;
-            }
-            if (mapa[i][j] == 79) {
-                mapa[i][j] = icon[k];
-            }
-            else {
-                //verificação se o jogador encontrou uma mumia;
-                if (mapa[i][j] == 2){
-                    char luta;
-                    cout << "Voce encontrou uma mumia quer tentar lutar com ela para roubar a sua bolsa de diamantes?(S)(N): ";
-                    cin >> luta;
-                    cout << endl;
-
-                    if (luta == 'S') {
-                        int lutando = rand() % 10 + 1;
-                        if (lutando >= 5) {
-                            int tesouro = rand() % 100 +1;
-                            if (tesouro == 100) {
-                                cout << "Voce derrotou a mumuia e achou um mapa do tesouro na bolsa dela +10000000000 quilates adicionados ao inventario." << endl;
-                                quilates[k]+=10000000000;
-                            }
-                            else {
-                                cout << "Voce derrotou a mumia e na bolsa dela tinha 1 diamante de 10 quilates." << endl;
-                                quilates[k]+=10;
-                            }
-                        }
-                        else {
-                            if (quilates[k] > 0){
-                                quilates[k]=0;
-                                cout << "A mumia te derrotou e roubou todos os seus diamantes" << endl;
-                            }
-                            else{
-                                cout << "A mumia te derrotou" << endl;
-                            }
-                        }
-                        k++;
-                    }
-                    else {
-                        cout << "A mumia se afastou" << endl;
-                        mapa[i][j] = icon[k];
-                        k++;
-                    }
-
+        for (int i = 1; i < jogadores+1; i++) {
+            // verifica se o jogador escolheu uma casa existente;
+            bool coordenada = true;       
+            int line;
+            int colum;
+            while (true) {
+                cout << players[i] << " Escolha uma linha e uma coluna para explorar!!!" <<endl;
+                cin >> "Escolha linha: " >> line;
+                cin >> "Escolha coluna: " >> colum;
+                if (line > 6 || line < 0 || colum > 6 || colum < 0) {
+                    cout << "Escolha um numero de no maximo (5) e no minimo (0)" <<endl;
                 }
-                if (mapa[i][j] == 3){
-
+                else {
+                    // verifica se tem um jogador em determinada coordenada escolhida;
+                    for (int i = 1; i < jogadores+1; i++) {
+                        if (mapa[line][colum] == icon[i]) {
+                            cout << "Ja tem um jogador nessa coordenada! Escolha outra." << endl;
+                            coordenada = false;
+                            break;
+                        }
+                    }                    
+                }
+                if (coordenada) {
+                    // verifica se a coordenada e valida
+                    break;
                 }
             }
-        }
 
+            // eventos
+            switch (mapa[line][colum]) {
+            case 2:
+                mumia(i);
+                cout << players[i] << " Agora tem " << quilates[i] << " em seu inventário." <<endl;
+                mapa[line][colum]=icon[i];
+                break;
+            case 3:
+                pedra(i);
+                cout << players[i] << " Agora tem " << quilates[i] << " em seu inventário." <<endl;
+                break;
+            case 4:
+                espinhos(i);
+                cout << players[i] << " Agora tem " << quilates[i] << " em seu inventário." <<endl;
+                break;
+            case 5:
+
+                cout << players[i] << " Agora tem " << quilates[i] << " em seu inventário." <<endl;
+                break;
+            case 36:
+                diamante(i);
+                cout << players[i] << " Agora tem " << quilates[i] << " em seu inventário." <<endl;
+                break;
+            default:
+                break;
+            }
         }
+        rodadas++;
     }
     
-    
 }
-    /*for (int i = 0 ; i < linhas; i ++) {
-        for (int j = 0 ; j < colunas; j ++) {
-            cout << setw(3) << mapa[i][j];
-        }
-        cout << endl;
-    }*/
+    // for (int i = 0 ; i < linhas; i ++) {
+    //     for (int j = 0 ; j < colunas; j ++) {
+    //         cout << setw(3) << mapa[i][j];
+    //     }
+    //     cout << endl;
+    // }
---
+++ mm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    char mumia = 2;
+    char pedra = 3;
+    char espinho = 4;
+    char pista = 5;
+    char diamante = 36;
+    cout << mumia << " mumia" << endl;
+    cout << pedra << " pedra" << endl;
+    cout << espinho << " espinho" << endl;
+    cout << pista << " pista" << endl;
+    cout << diamante << " diamante" << endl;
+
+}
